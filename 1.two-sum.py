734ae6a3d85f123f3815f5b75e4cbd34f890e1da
AjayLiu <ajayliu2928@gmail.com>
2021-03-28 21:26:03 -0700

    completed two-sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -8,6 +8,21 @@
 from typing import List
 
 class Solution:
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        print("HI")
+        seen = []
+        for i in range(len(nums)-1):
+            num = nums[i]
+            if(not num in seen):
+                seen.append(num)
+                complement = target - num
+                try:
+                    complement_index = nums.index(complement, i+1)
+                except:
+                    complement_index = -1
+
+                if(complement_index != -1):
+                    return [i, complement_index]
+    
+    # twoSum([], [1,2,2,3,4,1,4,2], 5)
 # @lc code=end
