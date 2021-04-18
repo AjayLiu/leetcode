413976d7bcb4837d5584ce521743c9f1deb82e7a
AjayLiu <ajayliu2928@gmail.com>
2021-04-17 23:08:04 -0700

    O(1) space improvement but python hogs so much memory sheesh

--- 53.maximum-subarray.py
+++ 53.maximum-subarray.py
@@ -8,17 +8,18 @@
 from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        dp = [] # largest sum so far
+        best_so_far = nums[0]
+        prev_best = nums[0]
         for i in range(len(nums)):
-            if i == 0:
-                dp.append(nums[0])
-            else:
-                dp.append(max(dp[i-1] + nums[i], nums[i]))
-        return max(dp)
+            if i != 0:
+                prev_best = max(prev_best + nums[i], nums[i])
+                best_so_far = max(best_so_far, prev_best)
+        return best_so_far
 
 # print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
 # print(Solution().maxSubArray([-2,1,-3]))
 # print(Solution().maxSubArray([1,2,-1]))
 # print(Solution().maxSubArray([5,4,-1,7,8]))
+
 # @lc code=end
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-[5,4,-1,7,8]
+print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(Solution().maxSubArray([-2,1,-3]))
+print(Solution().maxSubArray([1,2,-1]))
+print(Solution().maxSubArray([5,4,-1,7,8]))
